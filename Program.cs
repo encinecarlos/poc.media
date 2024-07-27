596473cf6ed628fc2a3019dc7518d6098d128307
Carlos Encine <carlos.encine@outlook.com>
2024-07-26 22:44:02 -0300

    feat: start project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var nota1 = 5.2;
+var nota2 = 6.4;
+var nota3 = 7.3;
+
+var media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"Média final: {Convert.ToInt32(media)}");
+
+if (media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else if (media >= 5 && media < 7)
+{
+    Console.WriteLine("Recuperacção");
+} else
+{
+   Console.WriteLine("Reprovado");
+}
